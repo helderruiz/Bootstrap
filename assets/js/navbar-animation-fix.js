f18127e259fe008666da193f0dfe00c98e2fc220
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2022-09-23 16:25:57 -0300

    termino do projeto
    Utilizando Bootstrap

--- assets/js/navbar-animation-fix.js
+++ assets/js/navbar-animation-fix.js
@@ -1,8 +1,8 @@
 $('#collapse-navbar').on('show.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, 10%');
+	$('.topcasafina-banner').css('transform', 'translate(-50%, 10%');
 
 });
 
 $('#collapse-navbar').on('hide.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%');
+	$('.topcasafina-banner').css('transform', 'translate(-50%, -50%');
 });
